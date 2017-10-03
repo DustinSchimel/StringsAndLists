bc882e5573231d18c296449a739dc26219e6472e
Dustin Schimel <dustin.schimel@gmail.com>
2017-10-03 12:41:52 -0600

    Added javadoc docmentation to the GUI

--- src/stringsAndLists/view/StringsAndListsDisplay.java
+++ src/stringsAndLists/view/StringsAndListsDisplay.java
@@ -2,13 +2,27 @@ package stringsAndLists.view;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Simple GUI class using JOptionPane.
+ * @author Dustin Schimel
+ * @version 1.0 03.10.2017
+ */
 public class StringsAndListsDisplay
 {
+	/**
+	 * Displays a popup showing a supplied String to the user.
+	 * @param textToDisplay The text to be displayed.
+	 */
 	public void displayText(String textToDisplay)
 	{
 		JOptionPane.showMessageDialog(null, textToDisplay);
 	}
 	
+	/**
+	 * Uses a popup to display a question to the user and stores their response as a String.
+	 * @param questionToDisplay The question being asked.
+	 * @return The users response to the question.
+	 */
 	public String getResponse(String questionToDisplay)
 	{
 		String answer = "";
