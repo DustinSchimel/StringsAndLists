74e16e9a89688203c6991089158f56e8804e329f
Dustin Schimel <dustin.schimel@gmail.com>
2017-10-05 13:17:59 -0600

    Changed constructor parameters, added a data member.

--- src/stringsAndList/controller/StringsAndListsController.java
+++ src/stringsAndList/controller/StringsAndListsController.java
@@ -26,19 +26,38 @@ public class StringsAndListsController
 	
 	private void showTheList()
 	{
+		String currentCreator = "";
 		for (int index = 0; index < myKahoots.size(); index++)
 		{
+			currentCreator = myKahoots.get(index).getCreator();
+			
+			Kahoot currentKahoot = myKahoots.get(index);
+			String creator = currentKahoot.getCreator();
+			
 			popup.displayText(myKahoots.get(index).toString());
+			
+			if (currentCreator.equals("Nobody"))
+			{
+				for (int loop = 0; loop < 5; loop += 1)
+				{
+					popup.displayText("wow nobody does a lot");
+				}
+			}
+			
+			for (int currentLetterIndex = 0; currentLetterIndex < currentCreator.length(); currentLetterIndex += 1)
+			{
+				popup.displayText(currentCreator.substring(currentLetterIndex, currentLetterIndex + 1));
+			}
 		}
 	}
 	
 	private void fillTheList()
 	{
-		Kahoot fiftyStates = new Kahoot("Kashish", 50);	//Creates a new class with the inserted values
-		Kahoot mySecondKahoot = new Kahoot("Ethan", 2);
-		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE);
-		Kahoot animalColor = new Kahoot("Branton", 10);
-		Kahoot presidents = new Kahoot("Obama", 44);
+		Kahoot fiftyStates = new Kahoot("Kashish", 50, "The fifty United States");	//Creates a new class with the inserted values
+		Kahoot mySecondKahoot = new Kahoot("Ethan", 2, "The Double data type");
+		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE, "Everything");
+		Kahoot animalColor = new Kahoot("Branton", 10, "All the colors of the animals");
+		Kahoot presidents = new Kahoot("Obama", 44, "The 44th president of the US");
 		
 		myKahoots.add(fiftyStates);
 		myKahoots.add(mySecondKahoot);
--- src/stringsAndLists/model/Kahoot.java
+++ src/stringsAndLists/model/Kahoot.java
@@ -6,19 +6,23 @@ public class Kahoot
 	private int level;
 	private String creator;
 	private boolean isJumble;
+	private String topic;
 	
 	public Kahoot()
 	{
-		this.questionCount = 0;
+		this.questionCount = 0;	//.this refers to the current structure
 		this.level = 1;
 		this.creator = "Nobody";
 		this.isJumble = false;
+		this.topic = "some topic";
 	}
 	
-	public Kahoot(String creator, int questionCount)	//sets the values of the current class to the ones given to it
+	public Kahoot(String creator, int questionCount, String topic)	//sets the values of the current class to the ones given to it
 	{
+		this(); 	//will do the method above first
 		this.questionCount = questionCount;
 		this.creator = creator;
+		this.topic = topic;
 	}
 	
 	public String toString()
